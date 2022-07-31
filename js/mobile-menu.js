499ea2e42f00f2e63538194cbc8d8cbf8f4e1b79
Igor Smorodsky <ingerard@gmail.com>
2022-07-31 12:49:58 +0300

    mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,16 @@
 (() => {
+  // Button handle
   const menuBtnRef = document.querySelector("[data-menu-button]");
+  // Menu handle
   const mobileMenuRef = document.querySelector("[data-menu]");
 
+  // Add event on the Button click
   menuBtnRef.addEventListener("click", () => {
 
+    // Check attr aria-expanded and set to const True or False
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
+    
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
