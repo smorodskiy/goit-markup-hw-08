3968c185510e64288e00b5d274592df0f845cdbf
Igor Smorodsky <ingerard@gmail.com>
2022-08-02 13:36:08 +0300

    added adaptive img

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,20 +1,28 @@
 (() => {
-  // Button handle
-  const menuBtnRef = document.querySelector("[data-menu-button]");
+  // Open menu handle
+  const menuOpenBtnRef = document.querySelector("[data-menu-open]");
+  // Close menu handle
+  const menuCloseBtnRef = document.querySelector("[data-menu-close]");
   // Menu handle
   const mobileMenuRef = document.querySelector("[data-menu]");
 
-  // Add event on the Button click
-  menuBtnRef.addEventListener("click", () => {
+  const toggleFunc = () => {
 
     // Check attr aria-expanded and set to const True or False
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-    
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    const expanded = mobileMenuRef.getAttribute("aria-expanded") === "true" || false;
+    console.log(expanded);
 
     mobileMenuRef.classList.toggle("is-open");
-    
-  });
+    mobileMenuRef.setAttribute("aria-expanded", !expanded);
+
+    const scrollLock = !expanded
+    ? 'hidden'
+    : '';
+    document.body.style.overflow = scrollLock;
+  }
+
+  // Add event on the Buttons click
+  menuOpenBtnRef.addEventListener("click", toggleFunc);
+  menuCloseBtnRef.addEventListener("click", toggleFunc);
+  
 })();
